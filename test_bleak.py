1288772fde7afc7e8d12ec169bc48f28bdd68ef1
ptenbrock <prbs48@gmail.com>
2022-01-27 00:46:21 +0100

    Further tests

--- test_bleak.py
+++ test_bleak.py
@@ -39,42 +39,30 @@ def battery_indicator(char_handle, data):
 async def connection(address):
     async with BleakClient(address) as client:
         print("Connected")
-        # answer = await client.read_gatt_char(int("0x0013", 16))
-        # answer1 = await client.read_gatt_char(12)
-        # answer2 = await client.read_gatt_char(18)
-        # answer = await client.read_gatt_char()
-        # answer4 = await client.read_gatt_char()
-        # indicate_req = bytes([0x20, 0x00])
-        # answer5 = await client.write_gatt_descriptor("c7e70012-c847-11e6-8175-8c89a55d403c",
-        #                                        indicate_req)
-        services = await client.get_services()
+        # services = await client.get_services()
 
         active_side_uuid = "c7e70012-c847-11e6-8175-8c89a55d403c"
+        battery_uuid = '00002a19-0000-1000-8000-00805f9b34fb'
         model_number_uuid = '00002a24-0000-1000-8000-00805f9b34fb'
         serial_number_uuid = '00002a25-0000-1000-8000-00805f9b34fb'
         firmware_revision_uuid = '00002a26-0000-1000-8000-00805f9b34fb'
 
         answer5 = await client.start_notify(active_side_uuid, active_side_indicator)
-        # answer6 = await client.start_notify(active_side_uuid, battery_indicator)
-
-        # active_side_char = services.get_characteristic(active_side_uuid)
-        # active_side_descriptor = active_side_char.descriptors[0]
+        answer6 = await client.start_notify(battery_uuid, battery_indicator)
 
         answer1 = await client.read_gatt_char(firmware_revision_uuid)
         answer2 = await client.read_gatt_char(model_number_uuid)
         answer3 = await client.read_gatt_char(serial_number_uuid)
 
-        # answer3 = await client.read_gatt_descriptor(active_side_descriptor.handle)
         answer4 = await client.read_gatt_char(active_side_uuid)
-        # answer5 = await client.write_gatt_descriptor(active_side_descriptor.handle,
-        #                                              indicate_req)
+        #answer7 = await client.read_gatt_char(battery_uuid)
 
-        await asyncio.sleep(5.0)
+        await asyncio.sleep(25.0)
 
 def main():
     asyncio.run(discover())
-    # asyncio.run(connection(dev.address), debug = True)
-    asyncio.run(connection("C2:0D:2F:B3:D8:FF"), debug = True)
+    asyncio.run(connection(dev.address), debug = True)
+    # asyncio.run(connection("C2:0D:2F:B3:D8:FF"), debug = True)
 
 
 
